5b65b96b58309dd7bc031500d699287aee09a0e7
innosoltech <innovativesolgh@gmail.com>
2025-01-21 20:49:34 +0000

    updates made

--- frontend/src/components/AppBanner.jsx
+++ frontend/src/components/AppBanner.jsx
@@ -4,12 +4,12 @@ const AppBanner = () => {
   return (
     <div
       style={{
-        background: "rgba(0, 0, 0, 0.3)",
+        background: "rgba(0, 0, 0)",
         textAlign: "center",
         height: "25px",
       }}
     >
-      <p style={{ fontSize: "small" }}>
+      <p style={{ fontSize: "small", color: "rgb(255, 255, 255)" }}>
         Open everyday, from 9am to 11pm. Closed on Tuesdays.
         {/* <Link to="/info">
           <span style={{ color: "blue" }}>See here</span>

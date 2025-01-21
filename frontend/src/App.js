b5b3b900315fc9c83daac1c83fee9e4b78df8127
innosoltech <innovativesolgh@gmail.com>
2025-01-21 20:26:44 +0000

    updates made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,14 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import Header from "./components/Header";
-// import AppBanner from "./components/AppBanner";
+import AppBanner from "./components/AppBanner";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
-      {/* <AppBanner /> */}
+      <AppBanner />
       <main className="py-3 body-colour">
         <Container>
           <Outlet />
--- frontend/src/components/AppBanner.jsx
+++ frontend/src/components/AppBanner.jsx
@@ -4,16 +4,16 @@ const AppBanner = () => {
   return (
     <div
       style={{
-        background: "rgba(0, 0, 255, 0.3)",
+        background: "rgba(0, 0, 0, 0.3)",
         textAlign: "center",
         height: "25px",
       }}
     >
       <p style={{ fontSize: "small" }}>
-        Want to learn more about Agava?{" "}
-        <Link to="/info">
+        Open everyday, from 9am to 11pm. Closed on Tuesdays.
+        {/* <Link to="/info">
           <span style={{ color: "blue" }}>See here</span>
-        </Link>
+        </Link> */}
       </p>
     </div>
   );

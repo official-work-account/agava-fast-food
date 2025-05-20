4e1c738f912c1ca2e0b9bf2338d1936bf64e2eb6
innosoltech <innovativesolgh@gmail.com>
2025-05-20 18:24:36 +0000

    updates made

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -4,27 +4,47 @@ import Loader from "./Loader";
 import Message from "./Message";
 import { useGetTopProductsQuery } from "../slices/productsApiSlice";
 
-const ProductCarousel = () => {
-  const { data: products, isLoading, error } = useGetTopProductsQuery();
+// const ProductCarousel = () => {
+//   const { data: products, isLoading, error } = useGetTopProductsQuery();
 
-  return isLoading ? (
-    <Loader />
-  ) : error ? (
-    <Message variant="danger">{error}</Message>
-  ) : (
-    <Carousel pause="hover" className="bg-primary mb-4">
-      {products.map((product) => (
-        <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
-            <Carousel.Caption className="carousel-caption">
-              <h4>
-                {product.name} (₵{product.price})
-              </h4>
-            </Carousel.Caption>
-          </Link>
-        </Carousel.Item>
-      ))}
+//   return isLoading ? (
+//     <Loader />
+//   ) : error ? (
+//     <Message variant="danger">{error}</Message>
+//   ) : (
+//     <Carousel pause="hover" className="bg-primary mb-4">
+//       {products.map((product) => (
+//         <Carousel.Item key={product._id}>
+//           <Link to={`/product/${product._id}`}>
+//             <Image src={product.image} alt={product.name} fluid />
+//             <Carousel.Caption className="carousel-caption">
+//               <h4>
+//                 {product.name} (₵{product.price})
+//               </h4>
+//             </Carousel.Caption>
+//           </Link>
+//         </Carousel.Item>
+//       ))}
+//     </Carousel>
+//   );
+// };
+
+const ProductCarousel = () => {
+  // const { data: products, isLoading, error } = useGetTopProductsQuery();
+
+  return (
+    <Carousel pause="hover" className="bg-primary mb-5">
+      <Carousel.Item>
+        <Image src="/images/carousel/1.png" alt="food-image" />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <Image src="/images/carousel/2.png" alt="food-image" />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <Image src="/images/carousel/3.png" alt="food-image" />
+      </Carousel.Item>
     </Carousel>
   );
 };
